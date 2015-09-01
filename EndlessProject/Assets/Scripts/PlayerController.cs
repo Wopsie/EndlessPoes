e8e3ec3cfc5ea8683aa53f15833c95aba970b368
Sebastiaan <Buwalda@MacBook-Pro-van-Buwalda.local>
2015-09-01 21:39:58 +0200

    Added the basics for the player

---
+++ EndlessProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float jumpForce = 10f;
+	private Rigidbody2D myRB;
+	// Use this for initialization
+	void Start () 
+	{
+		GetTheComponents();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		GetButtonInput();
+	}
+
+	void GetButtonInput ()
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			//this.myRB.AddForce (Vector2.up * jumpForce);
+		}
+	}
+
+	void GetTheComponents()
+	{
+		myRB = GetComponent<Rigidbody2D>();
+	}
+
+}
