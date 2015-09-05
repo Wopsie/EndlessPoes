8a3f423781a67de1c90e830999ab4c3069419c69
Sebastiaan <Buwalda@MacBook-Pro-van-Buwalda.local>
2015-09-05 12:17:49 +0200

    Jump V2
    Squashed some bugs, tried to make it feel less floaty, still far from
    perfect though.

--- EndlessProject/Assets/Scripts/PlayerController.cs
+++ EndlessProject/Assets/Scripts/PlayerController.cs
@@ -6,8 +6,11 @@ public class PlayerController : MonoBehaviour {
 	private Rigidbody2D myRB;
 	
 	public float jumpForce = 10f;
+	public float initialJumpForce = 100f;
 	private float elapsedTime = 0f;
 	public float maxExtraJump = 5f;
+
+	private bool amIgrounded = false;
 	
 
 	void Start () 
@@ -28,7 +31,36 @@ public class PlayerController : MonoBehaviour {
 		}
 		if (Input.GetKey(KeyCode.Space) && elapsedTime < maxExtraJump && myRB.velocity.y >= 0)
 		{
+			if(amIgrounded == false)
+			{
 			myRB.AddForce(new Vector2(0,jumpForce+(elapsedTime)));
+			}
+			else
+			{
+			myRB.AddForce(new Vector2(0,initialJumpForce));
+				Debug.Log("DOING IT");
+			}
+		}
+		if (amIgrounded == false && !Input.GetKey(KeyCode.Space))
+		{
+			elapsedTime = maxExtraJump;
+		}
+	}
+
+	void OnCollisionEnter2D (Collision2D col)
+	{
+		if(col.gameObject.name == "TestFloor")
+		{
+			amIgrounded = true;
+			elapsedTime = 0;
+		}
+	}
+
+	void OnCollisionExit2D (Collision2D col)
+	{
+		if(col.gameObject.name == "TestFloor")
+		{
+			amIgrounded = false;
 		}
 	}
 
@@ -36,14 +68,7 @@ public class PlayerController : MonoBehaviour {
 	{
 		if (Input.GetKey(KeyCode.Space))
 		{
-			elapsedTime += Time.deltaTime*12;
-		}
-		else
-		{
-			if (myRB.velocity.y == 0)
-			{
-				elapsedTime = 0;
-			}
+			elapsedTime += Time.deltaTime*20;
 		}
 	}
 
