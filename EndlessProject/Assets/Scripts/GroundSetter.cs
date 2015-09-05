76e7060928ddee6aebfbfa080cefa5490e4b8741
Wopsie <joaghim.beltman@gmail.com>
2015-09-06 00:07:19 +0200

    Added looping Parallax background & ground loop at random height

--- EndlessProject/Assets/Scripts/GroundSetter.cs
+++ EndlessProject/Assets/Scripts/GroundSetter.cs
@@ -3,13 +3,28 @@ using System.Collections;
 
 public class GroundSetter : MonoBehaviour {
 
+    public float groundSpeed;
+    public float groundRespawnCords;
+    public float offScreenCords;
+    //private number groundHeight = Random.Range(-7.72f,-2.03f);
+
 	// Use this for initialization
-	void Start () {
+	void Start () 
+    {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () 
+    {
+        transform.Translate(Vector2.left * groundSpeed);
+
+        var groundHeight = Random.Range(-7.72f, -2.03f);
+        if (transform.position.x <= offScreenCords)
+        {
+            transform.position = new Vector3(groundRespawnCords, groundHeight, 0f);
+            //transform.position = new Vector3((groundRespawnCords-offScreenCords), groundHeight, 0f);
+            //transform.Translate(groundRespawnCords, groundHeight, 0f);
+        }
 	}
 }
--- EndlessProject/Assets/Scripts/Parallaxer.cs
+++ EndlessProject/Assets/Scripts/Parallaxer.cs
@@ -3,14 +3,19 @@ using System.Collections;
 
 public class Parallaxer : MonoBehaviour 
 {
-    public float buildingSpeed = 1f;
+    public float buildingSpeed;
+    public float bgRespawnCords = 12.6f;
 
     void FixedUpdate()
     {
         //transform.position = new Vector2()
         transform.Translate(Vector2.left * buildingSpeed);
-        Debug.Log(transform);
-        Debug.Log(transform.position.x);
+        //Debug.Log(transform);
+        //Debug.Log(transform.position.x);
+        if(transform.position.x <= -12.6)
+        {
+            transform.position = new Vector3(bgRespawnCords, -1.22f, 0f);
+        }
     }
 
     void Update()
