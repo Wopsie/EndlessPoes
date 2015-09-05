d2c0edc677e2cd88e12ba96c3d41d7869941feb7
Wopsie <joaghim.beltman@gmail.com>
2015-09-05 12:53:36 +0200

    added rough parallax effect & back/midground sprites

---
+++ EndlessProject/Assets/Scripts/Parallaxer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallaxer : MonoBehaviour 
+{
+    public float buildingSpeed = 1f;
+
+    void FixedUpdate()
+    {
+        //transform.position = new Vector2()
+        transform.Translate(Vector2.left * buildingSpeed);
+        Debug.Log(transform);
+        Debug.Log(transform.position.x);
+    }
+
+    void Update()
+    {
+
+    }
+}
