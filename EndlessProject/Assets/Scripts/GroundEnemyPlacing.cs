685928abce76ed556bfe846c1dff7c8505e60fa8
Wopsie <joaghim.beltman@gmail.com>
2015-09-08 08:36:24 +0200

    dont remember... wha...?

---
+++ EndlessProject/Assets/Scripts/GroundEnemyPlacing.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundEnemyPlacing : GroundSetter
+{
+
+    //private float gEnemyRespawncords = groundHeight + 6f;
+    private float gEnemyOffScreenCords = Random.Range(-12.3f, -50f);
+    private float groundEnemySpeed = 0.3f;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+        transform.Translate(Vector2.left * groundEnemySpeed);
+
+        
+	}
+}
