505fb8814d75b6f6e2703d98c820386d7708769f
Sebastiaan <Buwalda@mbp-van-buwalda.media.local>
2015-09-15 14:32:28 +0200

    Daily upkeep

--- EndlessProject/Assets/Scripts/DestroyAfterAWhile.cs
+++ EndlessProject/Assets/Scripts/DestroyAfterAWhile.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class DestroyAfterAWhile : MonoBehaviour {
 	float timeLeft = 1f;
 	void Update () {
-		Debug.Log(timeLeft);
 		timeLeft -= Time.deltaTime;
 		if ( timeLeft < 0 )
 		{
