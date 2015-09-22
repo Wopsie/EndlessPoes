309b062086a12744707e00ff6580744ebf981efb
Wopsie <joaghim.beltman@gmail.com>
2015-09-22 09:47:37 +0200

    added splashscreen

---
+++ EndlessProject/Assets/Scripts/ProjectPoes.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectPoes : MonoBehaviour {
+
+    public int SpeedLimit = 0;
+
+	// Use this for initialization
+	void Start () 
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        SpeedLimit++;
+        transform.Translate(Vector3.back * Time.deltaTime * (SpeedLimit/4), Space.World);
+
+        //Debug.Log(SpeedLimit);
+        if (Input.anyKey || SpeedLimit == 100)
+        {
+            Application.LoadLevel("MenuScene");
+        }
+	}
+    void FixedUpdate()
+    {
+
+    }
+}
---
+++ EndlessProject/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneChanger : MonoBehaviour {
+	
+	// Update is called once per frame
+	public void ChangeToScene (int sceneToChangeTo) 
+    {
+        Application.LoadLevel(sceneToChangeTo);
+	}
+}
